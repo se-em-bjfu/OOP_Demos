397cccddb9c7e6c3783604a200cb847a8372f704
royqh1979 <royqh1979@gmail.com>
2018-10-22 15:10:44 +0800

    minor change

---
+++ src/bjfu/em/se/oop/d01encapsulation/Test.java
@@ -0,0 +1,16 @@
+package bjfu.em.se.oop.d01encapsulation;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		People p= new People();
+		p.walk();
+		p.walk();
+		p.walk();
+
+		p.moveRightFoot();
+		p.moveRightFoot();
+		p.moveRightFoot();		
+	}
+
+}
